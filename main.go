4a5c539b11d28c03372ef4b915be5d800b33539c
anuj389 <54616454+anuj389@users.noreply.github.com>
2026-03-26 13:07:30 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, OpenShift Practice!")
+	})
+
+	fmt.Println("Server running on port", port)
+	http.ListenAndServe(":"+port, nil)
+}
